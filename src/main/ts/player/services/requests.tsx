dc24c9201a787818b955c4260bb7ac80504527e6
muratsalakhov <muratsalakhov@gmail.com>
2021-04-19 23:28:11 +0300

    requests update

--- src/main/ts/player/services/requests.tsx
+++ src/main/ts/player/services/requests.tsx
@@ -4,7 +4,10 @@ const url:string = '';
 
 export const getScripts = () => axios.get(url + '/api/script/mongo').then(response => response.data);
 
-export const getChapters = () => axios.get(url + '/api/chapter/mongo').then(response => response.data);
+export const getScript = (id:string) => axios.get(url + '/api/script/mongo/' + id)
+    .then(response => response.data);
+
+/*export const getChapters = () => axios.get(url + '/api/chapter/mongo').then(response => response.data);
 
 export const getChapter = (id:string) => axios.get(url + '/api/chapter/mongo/' + id)
-    .then(response => response.data);
+    .then(response => response.data);*/
